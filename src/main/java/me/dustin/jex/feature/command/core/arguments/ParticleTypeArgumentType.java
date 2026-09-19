7b5a40481e7a6315f3d8d62bb2b3cb42da7734f5
BerryRock0 <mdenials128@gmail.com>
2026-09-19 11:37:03 +0000

    Update ParticleTypeArgumentType.java

--- src/main/java/me/dustin/jex/feature/command/core/arguments/ParticleTypeArgumentType.java
+++ src/main/java/me/dustin/jex/feature/command/core/arguments/ParticleTypeArgumentType.java
@@ -1,5 +1,8 @@
 package me.dustin.jex.feature.command.core.arguments;
 
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -8,50 +11,62 @@ import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.particle.ParticleType;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
+import net.minecraft.core.particles.ParticleType;
 
-public class ParticleTypeArgumentType implements ArgumentType<String> {
+import net.minecraft.text.Text;
+import net.minecraft.network.chat.Component;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+
+public class ParticleTypeArgumentType implements ArgumentType<String>
+{
 
     @Override
-    public String parse(StringReader reader) throws CommandSyntaxException {
+    public String parse(StringReader reader) throws CommandSyntaxException
+    {
         int argBeginning = reader.getCursor(); // The starting position of the cursor is at the beginning of the argument.
-        if (!reader.canRead()) {
+        if (!reader.canRead())
+        {
             reader.skip();
         }
-        while (reader.canRead() && reader.peek() != ' ') { // peek provides the character at the current cursor position.
+
+        while (reader.canRead() && reader.peek() != ' ')
+        {
             reader.skip(); // Tells the StringReader to move it's cursor to the next position.
         }
         String str = reader.getString().substring(argBeginning, reader.getCursor());
 
-        if (Registry.PARTICLE_TYPE.get(new Identifier(str)) != null) {
+        if (Registry.PARTICLE_TYPE.get(new Identifier(str)) != null)
+        {
             return str;
-        } else {
-            throw new SimpleCommandExceptionType(Text.of("Not a feature")).createWithContext(reader);
+        }
+        else
+        {
+            throw new SimpleCommandExceptionType(Component.literal("Not a feature")).createWithContext(reader);
         }
     }
 
-    public static ParticleTypeArgumentType particleType() {
+    public static ParticleTypeArgumentType particleType()
+    {
         return new ParticleTypeArgumentType();
     }
-    public static ParticleType<?> getParticleType(CommandContext<FabricClientCommandSource> commandContext, String string) throws CommandSyntaxException {
-        return Registry.PARTICLE_TYPE.get(new Identifier(commandContext.getArgument(string, String.class)));
+
+    public static ParticleType<?> getParticleType(CommandContext<FabricClientCommandSource> commandContext, String string) throws CommandSyntaxException
+    {
+        return Registry.PARTICLE_TYPE.get(new ResourceLocation(commandContext.getArgument(string, String.class)));
     }
 
     @Override
-    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
-        Registry.PARTICLE_TYPE.getIds().forEach(identifier -> {
-            builder.suggest(identifier.toString());
-        });
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder)
+    {
+        Registry.PARTICLE_TYPE.getIds().forEach(identifier -> {builder.suggest(identifier.toString());});
         return builder.buildFuture();
     }
 
     @Override
-    public Collection<String> getExamples() {
+    public Collection<String> getExamples()
+    {
         return ArgumentType.super.getExamples();
     }
 }
