c2f14379c38925a9cea0e18d973d8e755177ac12
BerryRock0 <mdenials128@gmail.com>
2026-09-19 11:01:16 +0000

    Update Vec3ArgumentType.java

--- src/main/java/me/dustin/jex/feature/command/core/arguments/Vec3ArgumentType.java
+++ src/main/java/me/dustin/jex/feature/command/core/arguments/Vec3ArgumentType.java
@@ -1,5 +1,10 @@
 package me.dustin.jex.feature.command.core.arguments;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -7,19 +12,17 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.concurrent.CompletableFuture;
+
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 
 import me.dustin.jex.feature.command.core.arguments.impl.DefaultPosArgument;
 import me.dustin.jex.feature.command.core.arguments.impl.LookingPosArgument;
 import me.dustin.jex.feature.command.core.arguments.impl.PosArgument;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+
+import net.minecraft.text.Text;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.CommandManager;
-import net.minecraft.text.Text;
-import net.minecraft.util.math.Vec3d;
 
 public class Vec3ArgumentType implements ArgumentType<PosArgument> {
    private static final Collection<String> EXAMPLES = Arrays.asList("0 0 0", "~ ~ ~", "^ ^ ^", "^1 ^ ^-5", "0.1 -0.5 .9", "~0.5 ~1 ~-5");
@@ -39,7 +42,7 @@ public class Vec3ArgumentType implements ArgumentType<PosArgument> {
       return new Vec3ArgumentType(centerIntegers);
    }
 
-   public static Vec3d getVec3(CommandContext<FabricClientCommandSource> context, String name) {
+   public static Vec3 getVec3(CommandContext<FabricClientCommandSource> context, String name) {
       return ((PosArgument)context.getArgument(name, PosArgument.class)).toAbsolutePos((FabricClientCommandSource)context.getSource());
    }
 
