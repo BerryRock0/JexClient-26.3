3bf509a57eb523684eaf73bd8815c7e8c1b89514
BerryRock0 <mdenials128@gmail.com>
2026-09-19 11:33:10 +0000

    Update EnchantmentArgumentType.java

--- src/main/java/me/dustin/jex/feature/command/core/arguments/EnchantmentArgumentType.java
+++ src/main/java/me/dustin/jex/feature/command/core/arguments/EnchantmentArgumentType.java
@@ -1,5 +1,9 @@
 package me.dustin.jex.feature.command.core.arguments;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -7,39 +11,38 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
 
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 
-public class EnchantmentArgumentType implements ArgumentType<Enchantment> {
+public class EnchantmentArgumentType implements ArgumentType<Enchantment>
+{
    private static final Collection<String> EXAMPLES = Arrays.asList("unbreaking", "silk_touch");
-   public static final DynamicCommandExceptionType UNKNOWN_ENCHANTMENT_EXCEPTION = new DynamicCommandExceptionType((object) -> Text.translatable("enchantment.unknown", new Object[]{object}));
+   public static final DynamicCommandExceptionType UNKNOWN_ENCHANTMENT_EXCEPTION = new DynamicCommandExceptionType((object) -> Component.translatable("enchantment.unknown", new Object[]{object}));
 
-   public static EnchantmentArgumentType enchantment() {
+   public static EnchantmentArgumentType enchantment()
+   {
       return new EnchantmentArgumentType();
    }
 
-   public static Enchantment getEnchantment(CommandContext<FabricClientCommandSource> context, String name) {
-      return (Enchantment)context.getArgument(name, Enchantment.class);
+   public static Enchantment getEnchantment(CommandContext<FabricClientCommandSource> context, String name)
+   {
+      return context.getArgument(name, Enchantment.class);
    }
 
-   public Enchantment parse(StringReader stringReader) throws CommandSyntaxException {
-      Identifier identifier = Identifier.fromCommandInput(stringReader);
-      return (Enchantment)Registry.ENCHANTMENT.getOrEmpty(identifier).orElseThrow(() -> {
-         return UNKNOWN_ENCHANTMENT_EXCEPTION.create(identifier);
-      });
+   public Enchantment parse(StringReader stringReader) throws CommandSyntaxException
+   {
+      ResourceLocation identifier = Identifier.fromCommandInput(stringReader);
+      return Registry.ENCHANTMENT.getOrEmpty(identifier).orElseThrow(() -> {return UNKNOWN_ENCHANTMENT_EXCEPTION.create(identifier);});
    }
 
-   public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
-      Registry.ENCHANTMENT.getIds().forEach(identifier -> {
-         builder.suggest(identifier.toString());
-      });
+   public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder)
+   {
+      Registry.ENCHANTMENT.getIds().forEach(identifier -> {builder.suggest(identifier.toString());});
       return builder.buildFuture();
    }
 
