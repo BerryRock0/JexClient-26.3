ae72333880c2f1476c1fa702f6bd9e2df4c87ed4
BerryRock0 <mdenials128@gmail.com>
2026-09-19 11:27:43 +0000

    Update EventDrawScreen.java

--- src/main/java/me/dustin/jex/event/render/EventDrawScreen.java
+++ src/main/java/me/dustin/jex/event/render/EventDrawScreen.java
@@ -5,7 +5,6 @@ import me.dustin.events.core.Event;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.renderer.MultiBufferSource;
 
 public class EventDrawScreen extends Event {
 
