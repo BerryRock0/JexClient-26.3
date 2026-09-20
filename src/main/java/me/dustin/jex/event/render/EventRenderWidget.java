3d60fe84190c9ec01b39cc001eb5bae40ddbac45
BerryRock0 <mdenials128@gmail.com>
2026-09-20 07:14:23 +0000

    Update EventRenderWidget.java

--- src/main/java/me/dustin/jex/event/render/EventRenderWidget.java
+++ src/main/java/me/dustin/jex/event/render/EventRenderWidget.java
@@ -6,15 +6,15 @@ import net.minecraft.client.gui.components.AbstractWidget;
 
 public class EventRenderWidget extends Event {
 
-    private final ClickableWidget abstractWidget;
+    private final AbstractWidget abstractWidget;
     private final GuiGraphics guiGraphics;
 
-    public EventRenderWidget(ClickableWidget abstractWidget, GuiGraphics guiGraphics) {
+    public EventRenderWidget(AbstractWidget abstractWidget, GuiGraphics guiGraphics) {
         this.abstractWidget = abstractWidget;
         this.guiGraphics = guiGraphics;
     }
 
-    public ClickableWidget getAbstractWidget() {
+    public AbstractWidget getAbstractWidget() {
         return abstractWidget;
     }
 
